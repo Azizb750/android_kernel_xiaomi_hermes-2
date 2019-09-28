b6b451c015813a52d31dc61856443f5b7f857c90
Azizb750 <azizb750@gmail.com>
2019-09-28 18:51:51 +0300

    fix typo and enable dt2w Also disable hotplug and balance cpu govs, as currently I can't fix them

--- drivers/misc/mediatek/keypad/kpd.c
+++ drivers/misc/mediatek/keypad/kpd.c
@@ -872,6 +872,7 @@ static int kpd_pdrv_probe(struct platform_device *pdev)
 #ifdef KPD_PMIC_RSTKEY_MAP
 	__set_bit(KPD_PMIC_RSTKEY_MAP, kpd_input_dev->keybit);
 #endif
+#endif
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
 	sweep2wake_setdev(kpd_input_dev);
